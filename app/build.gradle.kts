b4824b9bfd75218efa9b7b17374c8502bbfa956b
HiImMatthewN <matthewnantes33@gmail.com>
2024-01-22 19:30:39 +0800

    Added Additional Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
+    id("com.google.devtools.ksp")
     id("kotlin-parcelize")
     id("androidx.navigation.safeargs")
     id("dagger.hilt.android.plugin")
@@ -39,39 +39,64 @@ android {
     }
 
     buildFeatures {
-        dataBinding = true
         viewBinding = true
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-
-    val navVersion = "2.7.6"
+    //Dependency Injection
     val hiltVersion = "1.1.0"
     val daggerHiltVersion = "2.48.1"
-    val okhttpVersion = "4.12.0"
-    val retrofitVersion = "2.9.0"
-    val gsonVersion = "2.9.0"
 
     implementation("com.google.dagger:hilt-android:$daggerHiltVersion")
     implementation("com.google.dagger:hilt-compiler:$daggerHiltVersion")
     implementation("androidx.hilt:hilt-common:$hiltVersion")
     implementation("androidx.hilt:hilt-compiler:$hiltVersion")
+
+    //Network
+    val okhttpVersion = "4.12.0"
+    val retrofitVersion = "2.9.0"
+    val gsonVersion = "2.9.0"
+
     implementation("com.google.code.gson:gson:$gsonVersion")
     implementation("com.squareup.okhttp3:okhttp:$okhttpVersion")
     implementation("com.squareup.okhttp3:logging-interceptor:$okhttpVersion")
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
     implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+
+    //UI
+    val navVersion = "2.7.6"
+    val glideVersion = "4.13.0"
+    val materialVersion = "1.11.0"
+    val androidCoreVersion = "1.12.0"
+    val appCompatVersion = "1.6.1"
+    val constraintLayoutVersion = "2.1.4"
+
+    implementation("androidx.core:core-ktx:$androidCoreVersion")
+    implementation("androidx.appcompat:appcompat:$appCompatVersion")
+    implementation("com.google.android.material:material:$materialVersion")
+    implementation("androidx.constraintlayout:constraintlayout:$constraintLayoutVersion")
     implementation("androidx.navigation:navigation-fragment-ktx:$$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+    implementation("com.github.bumptech.glide:glide:$glideVersion")
+    ksp("com.github.bumptech.glide:ksp:$glideVersion")
+
+
+    //Local
+    val roomVersion = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+
+
+    //Testing
+    val junitVersion = "4.13.2"
+    val androidJUnitVersion = "1.1.5"
+    val espressoVersion = "3.5.1"
+    testImplementation("junit:junit:$junitVersion")
+    androidTestImplementation("androidx.test.ext:junit:$androidJUnitVersion")
+    androidTestImplementation("androidx.test.espresso:espresso-core:$espressoVersion")
 
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,6 @@ plugins {
 
     id("com.google.dagger.hilt.android") version "2.44" apply false
     id("androidx.navigation.safeargs") version "2.7.6" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
 
 }
